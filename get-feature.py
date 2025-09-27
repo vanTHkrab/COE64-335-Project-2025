23cc8bdc5f1fb615f517e22f9e7d767b638cf36e
vanTHkrab <vanth2548@gmail.com>
2025-09-27 21:09:50 +0700

    Update

--- get-feature.py
+++ get-feature.py
@@ -2,8 +2,7 @@ import joblib
 
 model = joblib.load("models/best_rf_model.joblib")
 
-# ถ้า Pipeline มีขั้นตอน preprocessing เช่น ColumnTransformer
 if hasattr(model, "feature_names_in_"):
     print("Features:", model.feature_names_in_)
 else:
-    print("โมเดลนี้ไม่เก็บชื่อ feature ไว้ (เช็คตอน preprocess)")
+    print("The model does not have feature names information.")
